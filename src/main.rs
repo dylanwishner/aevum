ceda9e60b9c2767fa232aec7c4b0adfdee23ac7c
Dylan Wishner <dylanwishner@gmail.com>
2020-09-11 18:53:47 -0700

    silence unused import

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod search;
 
 use search::Search;
+#[allow(unused_imports)]
 use std::panic;
 
 fn main() {
@@ -11,15 +12,19 @@ fn main() {
     let search = Search::new();
     let target: u32 = search.clock / (1 / search.timer_period);
 
+    find_matches(target);
+}
+
+fn find_matches(target: u32) {
     for i in 1u64..2_u64.pow(32) {
         for j in 1u64..2_u64.pow(16) {
             if i * j == target as u64 {
-                print(i - 1, j - 1);
+                print_match(i - 1, j - 1);
             }
         }
     }
 }
 
-fn print(arr: u64, psc: u64) {
+fn print_match(arr: u64, psc: u64) {
     println!("Prescaler: {}\tAuto-Reload: {}", psc, arr);
 }
