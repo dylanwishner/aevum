d0791a6056b12a29bcdebe34d08f4e1801e30df8
Dylan Wishner <dylanwishner@gmail.com>
2020-09-15 12:07:18 -0700

    arg to choose number of matches to calculate

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod search;
 use search::{Search, TimeMode};
 #[allow(unused_imports)]
 use std::panic;
+use std::process::exit;
 
 fn main() {
     // Suppresses panic output to stdout if in release mode
@@ -10,22 +11,36 @@ fn main() {
     panic::set_hook(Box::new(|_info| {}));
 
     let search = Search::new();
+
     let multiplier = match search.time_unit {
         TimeMode::Microseconds => 1000000,
         TimeMode::Milliseconds => 1000,
         TimeMode::Seconds => 1,
     };
 
-    let target: u32 = search.clock / (1 / search.timer_period / multiplier);
+    let target: u32 = (search.clock as f32
+        / (1_f32 / (search.timer_period as f32 / multiplier as f32) as f32))
+        as u32;
 
-    find_matches(target);
+    find_matches(target, &search);
 }
 
-fn find_matches(target: u32) {
+fn find_matches(target: u32, search: &Search) {
+    let mut num_matches = 0;
+
+    // Print target value if in debug mode
+    #[cfg(debug_assertions)]
+    println!("Searching for target: {}", target);
+
     for i in 1u64..2_u64.pow(16) {
         for j in 1u64..2_u64.pow(16) {
             if i * j == target as u64 {
+                num_matches += 1;
                 print_match(i - 1, j - 1);
+
+                if num_matches == search.num_results {
+                    exit(0);
+                }
             }
         }
     }
--- src/search.rs
+++ src/search.rs
@@ -2,13 +2,14 @@ use clap::{App, Arg};
 
 pub struct Search {
     pub clock: u32,
+    pub num_results: u32,
     pub timer_period: u32,
     pub time_unit: TimeMode,
 }
 
 impl Search {
     // TODO replace each arg with custom validators
-    #[allow(unused)]    // Silence unused variable (time_unit) warnings even though its read, possible rustc issue?
+    #[allow(unused)] // Silence unused variable (time_unit) warnings even though its read, possible rustc issue?
     pub fn new() -> Search {
         let app = App::new("STM32 Timer Calculator")
             .version("0.1.0")
@@ -19,34 +20,46 @@ impl Search {
                 .long("clock")
                 .help("The clock speed of the timer peripheral")
                 .default_value("8000000")
-                .required(false))
+                .required(false)
+                .takes_value(true))
             .arg(Arg::with_name("TIME")
                 .help("The length of the timer (default is in seconds")
                 .default_value("10")
-                .required(true))
+                .required(true)
+                .takes_value(true))
             .arg(Arg::with_name("EN_MICROSECONDS")
                 .short("u")
                 .long("microseconds")
                 .help("Set the specified timer period to be in microseconds")
                 .conflicts_with_all(&["EN_MILLISECONDS", "EN_SECONDS"])
-                .overrides_with_all(&["EN_MILLISECONDS", "EN_SECONDS"]))
+                .overrides_with_all(&["EN_MILLISECONDS", "EN_SECONDS"])
+                .takes_value(false))
             .arg(Arg::with_name("EN_MILLISECONDS")
                 .short("m")
                 .long("milliseconds")
-                .help("Set the specified timer period to be in milliseconds"))
+                .help("Set the specified timer period to be in milliseconds")
+                .takes_value(false))
             .arg(Arg::with_name("EN_SECONDS")
                 .short("s")
                 .long("seconds")
-                .help("Set the specified timer period to be in seconds"));
+                .help("Set the specified timer period to be in seconds")
+                .takes_value(false))
+            .arg(Arg::with_name("RESULTS")
+                .short("r")
+                .long("results")
+                .help("The number of results to return")
+                .default_value("5")
+                .takes_value(true));
 
         // Parse each parameter
         let matches = app.get_matches();
         let clock = matches.value_of("CLOCK SPEED").unwrap();
         let timer_period = matches.value_of("TIME").unwrap();
         let mut time_unit: TimeMode = TimeMode::Seconds;
+        let num_results = matches.value_of("RESULTS").unwrap();
 
         // Validate that all option values are of type u32
-        let args = vec![clock, timer_period];
+        let args = vec![clock, timer_period, num_results];
         for arg in args {
             match arg.parse::<u32>() {
                 Ok(_) => continue,
@@ -61,7 +74,7 @@ impl Search {
             time_unit = TimeMode::Microseconds
         } else if matches.is_present("EN_MILLISECONDS") {
             time_unit = TimeMode::Milliseconds
-        } else  {
+        } else {
             // Default to seconds
             time_unit = TimeMode::Seconds
         }
@@ -69,6 +82,7 @@ impl Search {
         // Return the search values after being parsed and verified
         Search {
             clock: clock.parse::<u32>().unwrap(),
+            num_results: num_results.parse::<u32>().unwrap(),
             timer_period: timer_period.parse::<u32>().unwrap(),
             time_unit,
         }
