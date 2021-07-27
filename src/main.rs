81c35f922f7e4abbb4c8289a27d1de324d1d62b9
Dylan Wishner <dylanwishner@gmail.com>
2021-07-26 18:52:57 -0700

    Moved app args to cli.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod cli;
 mod search;
 
 use search::{Search, TimeMode};
--- src/search.rs
+++ src/search.rs
@@ -1,4 +1,4 @@
-use clap::{App, Arg};
+use crate::cli;
 
 pub struct Search {
     pub clock: u32,
@@ -11,48 +11,8 @@ impl Search {
     // TODO replace each arg with custom validators
     #[allow(unused)] // Silence unused variable (time_unit) warnings even though its read, possible rustc issue?
     pub fn new() -> Search {
-        let app = App::new("Aevum")
-            .version("0.1.1")
-            .author("Dylan Wishner")
-            .about("Determine the prescaler and auto-reload register values for a specified period of time for STM32 MCU timer peripherals")
-            .arg(Arg::with_name("CLOCK SPEED")
-                .short("c")
-                .long("clock")
-                .help("The clock speed of the timer peripheral")
-                .default_value("8000000")
-                .required(false)
-                .takes_value(true))
-            .arg(Arg::with_name("TIME")
-                .help("The length of the timer (default is in seconds")
-                // .default_value("10")
-                .required(true)
-                .takes_value(true))
-            .arg(Arg::with_name("EN_MICROSECONDS")
-                .short("u")
-                .long("microseconds")
-                .help("Set the specified timer period to be in microseconds")
-                .conflicts_with_all(&["EN_MILLISECONDS", "EN_SECONDS"])
-                .overrides_with_all(&["EN_MILLISECONDS", "EN_SECONDS"])
-                .takes_value(false))
-            .arg(Arg::with_name("EN_MILLISECONDS")
-                .short("m")
-                .long("milliseconds")
-                .help("Set the specified timer period to be in milliseconds")
-                .takes_value(false))
-            .arg(Arg::with_name("EN_SECONDS")
-                .short("s")
-                .long("seconds")
-                .help("Set the specified timer period to be in seconds")
-                .takes_value(false))
-            .arg(Arg::with_name("RESULTS")
-                .short("r")
-                .long("results")
-                .help("The number of results to return")
-                .default_value("5")
-                .takes_value(true));
-
         // Parse each parameter
-        let matches = app.get_matches();
+        let matches = cli::matches();
         let clock = matches.value_of("CLOCK SPEED").unwrap();
         let timer_period = matches.value_of("TIME").unwrap();
         let mut time_unit: TimeMode = TimeMode::Seconds;
